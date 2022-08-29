7d6b8e6ea9333dd663ed371ccd0e682a477684a7
conchincradle <33627638+conchincradle@users.noreply.github.com>
2022-08-29 11:26:01 +0900

    Update transformToPuzzles.py

--- transformToPuzzles.py
+++ transformToPuzzles.py
@@ -1,3 +1,28 @@
+import random
+from google.colab.patches import cv2_imshow
+new_crop = crops.copy()
+random.shuffle(new_crop)
+#print(new_crop)
+
+img = [0]*n
+for i in range(n):
+  path = new_crop[n*i]
+  
+  img[i] = cv2.imread(path)
+for i in range(n):
+  for j in range(1,n):
+    path = new_crop[n*i+j]
+    img[i] = np.concatenate((img[i],cv2.imread(path)),axis=1)
+  #cv2_imshow(img[i])
+final = img[0]   
+for i in range(1,n):
+  final = np.concatenate((final,img[i]),axis=0)
+cv2_imshow(final)
+
+print(final.shape)
+cv2.imwrite("final.jpg",final)
+# final.jpg renamed to puzzle_128.jpg
+# below is to do image blending of the concatenated lines
 img = cv2.imread("puzzle_128.jpg")
 n = 128
 for i in range(1,n):
